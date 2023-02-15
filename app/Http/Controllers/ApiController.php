b08585b9391f9b838d4dd7846436d4330457de64
Kolobaev Maxim <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+Kolobok1995@users.noreply.github.com>
2023-02-16 02:10:37 +0300

    ntcn

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -6,8 +6,15 @@ use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Base\BaseApiController;
 
+
 class ApiController extends BaseApiController
 {
+    /**
+     * Возвращает ответ
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
     public function actionExchanger(Request $request): JsonResponse
     {
         $this->initExchanger($request->mode, $request->data);
